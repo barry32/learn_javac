e4fba5790d0d7a5827dfc324f54de161e16096bd
barry.liu <barry.liu@happyeasygo.com>
2020-10-26 14:14:56 +0800

    do tiny modification

---
+++ src/com/sun/tools/javac/HHH.java
@@ -0,0 +1,12 @@
+package com.sun.tools.javac;
+
+/**
+ * @Description
+ * @Author Barry
+ * @create: 2020-10-23 15:41
+ **/
+public class HHH {
+
+    public HHH() {
+    }
+}
--- src/com/sun/tools/javac/HelloWorld.java
+++ src/com/sun/tools/javac/HelloWorld.java
@@ -1,5 +1,8 @@
 package com.sun.tools.javac;
 
+
+import com.sun.org.glassfish.gmbal.Description;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Native;
 
@@ -8,9 +11,16 @@ import java.lang.annotation.Native;
  * @Author Barry
  * @create: 2020-10-21 23:39
  **/
-public class HelloWorld {
+@Description(value = "java")
+public class HelloWorld extends HHH{
+
+    public HelloWorld() {
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int i =0;
+        System.out.println(10/i);
         test();
     }
 
@@ -19,4 +29,14 @@ public class HelloWorld {
         System.out.println("test!");
     }
 
+
+    class TestA{
+
+        int i = 19;
+    }
+}
+
+class TestB{
+
+    int i = 19;
 }
